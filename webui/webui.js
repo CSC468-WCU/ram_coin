b7040332efe758ec103490355ad7906b778cc870
Linh B Ngo <lngo@wcupa.edu>
2025-04-17 22:16:17 -0400

    Update webui.js

--- webui/webui.js
+++ webui/webui.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var app = express();
 var redis = require('redis');
-var path = require('path');
-
 
 var client = redis.createClient(6379, 'redis');
 client.on("error", function (err) {
     console.error("Redis error", err);
 });
 
-app.use(express.static('__dirname'));
-
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.redirect('/index.html');
 });
 
 app.get('/json', function (req, res) {
@@ -28,8 +24,8 @@ app.get('/json', function (req, res) {
     });
 });
 
+app.use(express.static('files'));
 
 var server = app.listen(80, function () {
     console.log('WEBUI running on port 80');
 });
-
